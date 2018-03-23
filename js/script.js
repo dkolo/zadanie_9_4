bc7259984a66b5ac1226fe331ec6c5b897be4832
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-03-23 18:36:07 +0100

    Fix bug with condition in loop k

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var treeRows = prompt('Wpisz zakładaną liczbę pięter choinki');
 function drawTree() {
-	for ( var i = 0 ; i <= treeRows ; i++ ) {
+	for ( var i = 0 ; i < treeRows ; i++ ) {
 		var star = '',
 			 space = '';
 		for ( var j = 0 ; j < ( treeRows - i ) ; j++ ) {
 			space += ' ';
 		}
-		for ( var k = 0 ; k < (2 * i - 1) ; k++ ) {
+		for ( var k = 0 ; k < (2 * i + 1) ; k++ ) {
 			star += '*';
 		}
 	console.log(space + star);
