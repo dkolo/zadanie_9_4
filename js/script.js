3290a55c2c292eb32b3515605758a7124ce0bf45
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-03-23 18:26:26 +0100

    Add solution of full task 9.4 in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 var treeRows = prompt('Wpisz zakładaną liczbę pięter choinki');
 function drawTree() {
-	for ( var i = 0 ; i <= treeRows ; i++ ) {
-		var star = '';
-		for ( var j = 0 ; j < i ; j++ ) {
+	for ( var i = 0 ; i < treeRows ; i++ ) {
+		var star = '',
+			 space = '';
+		for ( var j = 0 ; j < ( treeRows - i ) ; j++ ) {
+			space += ' ';
+		}
+		for ( var k = 0 ; k < (2 * i - 1) ; k++ ) {
 			star += '*';
 		}
-	console.log(star);
+	console.log(space + star);
 	}
 }
 drawTree(treeRows);
